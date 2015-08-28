ac57f216c2f05df292490e4fff5a11f5aeddb8ef
KierstonHill83 <khill1218@gmail.com>
2015-08-28 16:30:49 -0600

    Added logic so each submission is added to the page

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,18 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var utilities = require('./utilities.js');
+
+var submissionArray = [];
+
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Github Contest' });
+});
+
+router.post('/submit', function(req, res, next) {
+
+var subName = req.body.personName;
+var subImage = req.body.personImage || 'https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png';
+var subURL = req.body.personUrl;
+
+  submissionArray.push({
+    name: subName,
+    image: subImage,
+    url: subURL
+  });
+
+console.log(submissionArray);
+
+  res.render('submission', {
+    title: 'All Submissions',
+    message: 'Your submission was successful!',
+    submissions: submissionArray
+  });
 });
 
 
+router.post('/', function(req, res, next) {
+  res.redirect('/');
+});
 
-
-function Submission(githubName, githubURL, githubImage) {
-  this.githubName = githubName;
-  this.githubURL = githubURL;
-  this.githubImage = githubImage;
-}
 
 
 module.exports = router;
---
+++ server/routes/utilities.js
@@ -0,0 +1,9 @@
+function Submission(githubName, githubURL, githubImage) {
+  this.githubName = githubName;
+  this.githubURL = githubURL;
+  this.githubImage = githubImage;
+  this.newSubmission = [];
+}
+
+
+module.exports = Submission;
